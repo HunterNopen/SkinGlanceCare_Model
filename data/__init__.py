8771c361c1bf6370b95f6f13ecf5af55ef0baefa
HunterNopen <angleelove6@gmail.com>
2026-01-20 17:22:46 +0100

    SGCM-23 - Introduced datamodule.py that packages ISIC2019 dataset + Augmentations

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,3 @@
-"""Data handling module for skin lesion classification."""
-
 from .dataset import ISICDataset
 from .datamodule import ISICDataModule
 from .samplers import CancerOversamplerFactory
