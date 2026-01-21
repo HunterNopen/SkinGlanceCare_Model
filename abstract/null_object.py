0344a25c4a6096857a41ab68bd23c607e4d59569
HunterNopen <angleelove6@gmail.com>
2026-01-21 10:37:29 +0100

    SGCM-27 - Refactored code consistency. Added NullObject pattern.

---
+++ abstract/null_object.py
@@ -0,0 +1,35 @@
+class NullObject:
+    """A Null Object that safely absorbs interactions."""
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+    def __getattr__(self, name):
+        return self
+
+    def __setattr__(self, name, value):
+        # Ignore any attribute assignments except internal
+        if name in {"_instance"}:
+            return super().__setattr__(name, value)
+        return self
+
+    def __bool__(self):
+        return False
+
+    def __len__(self):
+        return 0
+
+    def __iter__(self):
+        return iter(())
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        return False
