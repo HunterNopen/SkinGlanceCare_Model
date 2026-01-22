d9cb27388af8e7f7221aa630c845a54d7a1eb49a
HunterNopen <angleelove6@gmail.com>
2026-01-22 10:39:01 +0100

    SGCM-30 - Implemented dynamic config class for every submodule

---
+++ config/__init__.py
@@ -0,0 +1,19 @@
+from .config import (
+    Config,
+    ModelConfig,
+    TrainingConfig,
+    DataConfig,
+    LossConfig,
+    InferenceConfig,
+    LoggingConfig,
+)
+
+__all__ = [
+    "Config",
+    "ModelConfig",
+    "TrainingConfig",
+    "DataConfig",
+    "LossConfig",
+    "InferenceConfig",
+    "LoggingConfig",
+]
